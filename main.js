b2e3cd25feed36428a2a8d2df8b0c71f1d8c0eec
Starsign68 <37559540+Starsign68@users.noreply.github.com>
2022-06-06 22:07:37 -0400

    Fix sample text

--- main.js
+++ main.js
@@ -42,10 +42,7 @@ require(["vs/editor/editor.main"], function () {
  //  let Theme = "Pencil-simp";
  globalThis.theme = Theme;
  let txt = `This is a good start ... But is it really the best?
-This is a good test case. I will repeat all of the best examples to show that this is good, and not bad.
-You are starting to get it! Now, just try to find some of the errors in the data and then fix the not good math.
-Then, once you are done, add, then subtract most of the test results.
-Can you then please start over and describe what you just fixed? `;
+One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me?" he thought. It wasn't a dream. His room, a proper human room although a little too small, lay peacefully between its four familiar walls. A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame. It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather...`;
  let txt2 = `this is not jjust a great app ... It is an awesome app!!!`;
  // Define new themes that contain rules that match this language
  console.time("THEME");
